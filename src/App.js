98351807a4fc387332fdc15d741c1c6d4820d499
mourad <mourad.latoui.89@gmail.com>
2023-06-13 08:59:30 -0400

    html and css

--- src/App.js
+++ src/App.js
@@ -3,8 +3,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      <p>Hello</p>
+      <h1>Modal</h1>
+      <div class="container">
+        <button id="modal-1">Modal 1</button>
+        <button id="modal-2">Modal 2</button>
+        <button id="modal-3">Modal 3</button>
+        <button id="modal-4">Modal 4</button>
+        <button id="modal-5">Modal 5</button>
+      </div>
     </div>
   );
 }
